2bba7f254c83483b157b52ae7406b1eab5761328
Dmitry Nekrasov <scannorone@gmail.com>
2017-01-22 14:28:07 +0700

    Добавил элементы GUI на форму

--- src/signalprocessing/Lab03JFrame.java
+++ src/signalprocessing/Lab03JFrame.java
@@ -5,17 +5,30 @@
  */
 package signalprocessing;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.XYChart;
+
 /**
  *
  * @author ScanNorOne
  */
 public class Lab03JFrame extends javax.swing.JFrame {
 
+    final String seriesName = "y(x)";
+    
+    XYChart frChart;
+    
     /**
      * Creates new form Lab03JFrame
      */
     public Lab03JFrame() {
         initComponents();
+        
+        frChart = QuickChart.getChart("", "", "", seriesName, new double[1], new double[1]);
+        
+        setEEnabled(false);
     }
 
     /**
@@ -27,57 +40,115 @@ public class Lab03JFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        frPanel = new javax.swing.JPanel();
+        filterLabel = new javax.swing.JLabel();
+        filterComboBox = new javax.swing.JComboBox<>();
+        startButton = new javax.swing.JButton();
+        NLabel = new javax.swing.JLabel();
+        NTextField = new javax.swing.JTextField();
+        eLabel = new javax.swing.JLabel();
+        eTextField = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout frPanelLayout = new javax.swing.GroupLayout(frPanel);
+        frPanel.setLayout(frPanelLayout);
+        frPanelLayout.setHorizontalGroup(
+            frPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        frPanelLayout.setVerticalGroup(
+            frPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 200, Short.MAX_VALUE)
+        );
+
+        filterLabel.setText("Фильтр:");
+
+        filterComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Баттерворта", "Чебышева 1 типа", "Чебышева 2 типа" }));
+        filterComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                filterComboBoxActionPerformed(evt);
+            }
+        });
+
+        startButton.setText("Старт");
+
+        NLabel.setText("N:");
+
+        NTextField.setText("8");
+
+        eLabel.setText("e:");
+
+        eTextField.setText("0.5");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(frPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(filterLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(filterComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(NLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(NTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(eLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(eTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(startButton))
+                .addContainerGap(604, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(frPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(filterLabel)
+                    .addComponent(filterComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(NLabel)
+                    .addComponent(NTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(eLabel)
+                    .addComponent(eTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(startButton)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Lab03JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Lab03JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Lab03JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Lab03JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void filterComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filterComboBoxActionPerformed
+        setEEnabled(filterComboBox.getSelectedIndex() != 0);
+    }//GEN-LAST:event_filterComboBoxActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Lab03JFrame().setVisible(true);
-            }
-        });
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D frPanelGraphics = (Graphics2D) frPanel.getGraphics();
+        frChart.paint(frPanelGraphics, frPanel.getWidth(), frPanel.getHeight());
+    }    
+    
+    private void setEEnabled(boolean value) {
+        eLabel.setEnabled(value);
+        eTextField.setEditable(value);
     }
-
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel NLabel;
+    private javax.swing.JTextField NTextField;
+    private javax.swing.JLabel eLabel;
+    private javax.swing.JTextField eTextField;
+    private javax.swing.JComboBox<String> filterComboBox;
+    private javax.swing.JLabel filterLabel;
+    private javax.swing.JPanel frPanel;
+    private javax.swing.JButton startButton;
     // End of variables declaration//GEN-END:variables
 }
